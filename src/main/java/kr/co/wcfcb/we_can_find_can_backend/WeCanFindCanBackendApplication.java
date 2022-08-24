5775686b2ddc2818c607310f5683b7949fd1fcfc
applan <applan_siepe@naver.com>
2022-08-25 03:54:16 +0900

    2022-08-25 Spring Boot Project 생성

---
+++ src/main/java/kr/co/wcfcb/we_can_find_can_backend/WeCanFindCanBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.co.wcfcb.we_can_find_can_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeCanFindCanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeCanFindCanBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/wcfcb/we_can_find_can_backend/WeCanFindCanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wcfcb.we_can_find_can_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeCanFindCanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
