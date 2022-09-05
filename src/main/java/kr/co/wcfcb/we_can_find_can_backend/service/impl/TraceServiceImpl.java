5eb672b0774c898ab5f752c3d7d7c3773c81d142
applan <applan_siepe@naver.com>
2022-09-06 00:56:12 +0900

    [Core] 위치를 이용한 조회 비즈니스 로직 구현 (테스트 필요)

--- src/main/java/kr/co/wcfcb/we_can_find_can_backend/service/impl/TraceServiceImpl.java
+++ src/main/java/kr/co/wcfcb/we_can_find_can_backend/service/impl/TraceServiceImpl.java
@@ -1,10 +1,17 @@
 package kr.co.wcfcb.we_can_find_can_backend.service.impl;
 
+import co.elastic.clients.elasticsearch._types.GeoLocation;
+import co.elastic.clients.elasticsearch._types.LatLonGeoLocation;
+import co.elastic.clients.elasticsearch._types.query_dsl.GeoDistanceQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import kr.co.wcfcb.we_can_find_can_backend.dao.TraceDao;
 import kr.co.wcfcb.we_can_find_can_backend.domain.Location;
 import kr.co.wcfcb.we_can_find_can_backend.domain.Trace;
+import kr.co.wcfcb.we_can_find_can_backend.prop.ElasticsearchIndex;
 import kr.co.wcfcb.we_can_find_can_backend.service.TraceService;
+import kr.co.wcfcb.we_can_find_can_backend.util.ElasticsearchUtil;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,7 +27,16 @@ public class TraceServiceImpl implements TraceService {
     public List<Trace> findByLocation(Location location) {
         List<Trace> traceList = null;
         try{
-            SearchResponse<Trace> searchResponse = traceDao.findByLocation(null);
+            // https://www.elastic.co/guide/en/elasticsearch/client/java-api-client/current/searching.html
+            LatLonGeoLocation llgl = ElasticsearchUtil.caseLatLonGeoLocation(location);
+            GeoLocation gl = ElasticsearchUtil.caseGeoLocation(llgl);
+            Query gdq = GeoDistanceQuery.of(g -> g
+                    .distance("1km")
+                    .location(gl))._toQuery();
+            SearchRequest sr = SearchRequest.of(s -> s
+                    .index(ElasticsearchIndex.TRACE_INDEX)
+                    .query(gdq));
+            SearchResponse<Trace> searchResponse = traceDao.findByLocation(sr);
         }catch (Exception e) {
             e.printStackTrace();
         }
