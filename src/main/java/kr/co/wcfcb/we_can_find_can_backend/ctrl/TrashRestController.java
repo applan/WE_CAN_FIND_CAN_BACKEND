7c9a4f6f4c433151fb79a3e70fbbd1f61d598aa7
applan <applan_siepe@naver.com>
2022-09-12 19:48:55 +0900

    [Refactor] trash URL 종합

--- src/main/java/kr/co/wcfcb/we_can_find_can_backend/ctrl/TrashRestController.java
+++ src/main/java/kr/co/wcfcb/we_can_find_can_backend/ctrl/TrashRestController.java
@@ -3,11 +3,7 @@ package kr.co.wcfcb.we_can_find_can_backend.ctrl;
 import kr.co.wcfcb.we_can_find_can_backend.domain.Location;
 import kr.co.wcfcb.we_can_find_can_backend.domain.Trash;
 import kr.co.wcfcb.we_can_find_can_backend.service.TrashService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,24 +13,25 @@ import java.util.List;
  * @version 1.0
  */
 @RestController
+@RequestMapping("/trash")
 public class TrashRestController {
 
     private final TrashService trashService;
 
     public TrashRestController(TrashService trashService) {this.trashService = trashService;}
 
-    @GetMapping("/trash/get")
+    @GetMapping("/get")
     public List<Trash> findByLocation(@RequestParam() Location location) {
         return trashService.findByLocation(location);
     }
-    
-    @PutMapping("/trash/put")
+
+    @PutMapping("/put")
     public void addByLocation(@RequestParam() Location location) {
-    	trashService.addByLocation(location);
+        trashService.addByLocation(location);
     }
-    
-    @PostMapping("/trash/post")
+
+    @PostMapping("/post")
     public void updateByLocation(@RequestParam() Location location) {
-    	trashService.updateByLocation(location);
+        trashService.updateByLocation(location);
     }
 }
