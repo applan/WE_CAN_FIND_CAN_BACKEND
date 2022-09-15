e40a2c865bb9a7913cd1f1a2ec691965d36b18f7
applan <applan_siepe@naver.com>
2022-09-16 01:44:12 +0900

    [Simple] toString Override

--- src/main/java/kr/co/wcfcb/we_can_find_can_backend/domain/Location.java
+++ src/main/java/kr/co/wcfcb/we_can_find_can_backend/domain/Location.java
@@ -30,4 +30,12 @@ public class Location {
         this.lat = lat;
         this.lon = lon;
     }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "lat=" + lat +
+                ", lon=" + lon +
+                '}';
+    }
 }
