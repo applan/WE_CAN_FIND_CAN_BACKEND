7442a1d90413819f2e6e997c3eb2d421bda7656e
applan <applan_siepe@naver.com>
2022-09-17 03:55:13 +0900

    [Fix] ServletComponentScan 어노테이션 제거

--- src/main/java/kr/co/wcfcb/we_can_find_can_backend/WeCanFindCanBackendApplication.java
+++ src/main/java/kr/co/wcfcb/we_can_find_can_backend/WeCanFindCanBackendApplication.java
@@ -2,9 +2,7 @@ package kr.co.wcfcb.we_can_find_can_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan
 @SpringBootApplication
 public class WeCanFindCanBackendApplication {
 
